0262a3a22ab18087dc0ba58c86400f39513b0b1b
Luca Galati <memento.reloaded@gmail.com>
2016-05-27 11:05:09 +0200

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Luca',
+// 	age: 28
+// }
+
+// function updatePerson (obj)  {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 35;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+var grades = [50, 37];
+
+function addGrade (array) {
+	array.push(90);
+	debugger;
+};
+
+addGrade(grades);
+console.log(grades);
