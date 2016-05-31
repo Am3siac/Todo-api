f5d745c4f1053146eba616c9fb34a11affeabb40
Luca Galati <memento.reloaded@gmail.com>
2016-05-31 17:55:42 +0200

    Final static todos

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 });
 
 
-// GET /todos?completed=<boolean>
+// GET /todos?completed=<boolean>&q=<string>
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -27,6 +27,12 @@ app.get('/todos', function (req, res) {
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
